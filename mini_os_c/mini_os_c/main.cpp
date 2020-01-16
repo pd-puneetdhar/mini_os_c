57003ed75a30e386fbf6b57695d3854b80f85007
pd-puneetdhar <55623977+pd-puneetdhar@users.noreply.github.com>
2020-01-17 03:45:20 +0530

    stage main.cpp properly

--- mini_os_c/mini_os_c/main.cpp
+++ mini_os_c/mini_os_c/main.cpp
@@ -1,14 +1,10 @@
 #include "os_task.h"
 #include "os_api.h"
 #include "common.h"
-
 void user_fn0(void) { return; }
 int main() {
-
 	result _result = e_start;
-
 	os_task user_task0 = { 0, user_fn0 };
 	user_task0.task();
 	return 0;
-
 }
