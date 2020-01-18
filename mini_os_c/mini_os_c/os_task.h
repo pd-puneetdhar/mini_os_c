f8d194490b11cc40cf44aaa499ddf639d543f850
pd-puneetdhar <55623977+pd-puneetdhar@users.noreply.github.com>
2020-01-19 00:52:33 +0530

    Update os_task.h

--- mini_os_c/mini_os_c/os_task.h
+++ mini_os_c/mini_os_c/os_task.h
@@ -23,20 +23,27 @@ namespace ostask {
 	typedef void (*task)(status* s);
 
 	status complete(os_task* t) { t->_task(&t->_status); return t->_status; }
+	void reset(os_task* t) { t->_status = init; }
+	BOOL pending(os_task* t) { BOOL b = (t->_status == init) ? TRUE : FALSE; return b; }
 }
 
 namespace test {
 	using namespace ostask;
 
-	void fn0(status* s) { *s = ostask::completed; }
+	void fn0(status* s) { *s = ostask::completed; return; }
+	void fn1(status* s) { return; }
 
 	TEST(os_task) {
 
 		using namespace ostask;
 		os_task task_0 = { 0, fn0, init };
-		assert(task_0._status == ostask::init);
-		assert(complete(&task_0) == ostask::completed);
+		os_task* t = &task_0;
 
+		assert(t->_status == ostask::init);
+		assert(complete(t) == ostask::completed);
+		assert(pending(t) == FALSE);
+		reset(t);
+		assert(pending(t) == TRUE);
 
 		return;
 	}
