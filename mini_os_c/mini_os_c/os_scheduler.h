530816e5857eda1767aac934ccc244d49dc0cd20
pd-puneetdhar <55623977+pd-puneetdhar@users.noreply.github.com>
2020-01-20 00:24:45 +0530

    Update os_scheduler.h

--- mini_os_c/mini_os_c/os_scheduler.h
+++ mini_os_c/mini_os_c/os_scheduler.h
@@ -8,8 +8,13 @@ namespace ossch {
 	void run();
 
 	typedef struct os_scheduler {
-		os_queue m_queue;
+		os_queue* m_queue;
 	} os_scheduler;
+
+	os_task* schedule(os_scheduler* s) {
+		os_task* t = 0;
+		return t;
+	}
 
 }
 
