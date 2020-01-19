3aca3c7d53bf7c6d32ea0c464ec6dbed34f8f24c
pd-puneetdhar <55623977+pd-puneetdhar@users.noreply.github.com>
2020-01-19 10:15:02 +0530

    Update os_task.h

--- mini_os_c/mini_os_c/os_task.h
+++ mini_os_c/mini_os_c/os_task.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "common.h"
 
 namespace ostask {
@@ -22,9 +23,24 @@ namespace ostask {
 
 	typedef void (*task)(status* s);
 
-	status complete(os_task* t) { t->_task(&t->_status); return t->_status; }
 	void reset(os_task* t) { t->_status = init; }
 	BOOL pending(os_task* t) { BOOL b = (t->_status == init) ? TRUE : FALSE; return b; }
+	status complete(os_task* t) { t->_task(&t->_status); return t->_status; }
+
+	os_task* create_os_task(id _id, task _task) {
+		os_task* t = (os_task*)malloc(sizeof(os_task));
+		if (t != 0) {
+			t->_id = _id;
+			t->_task = _task;
+			t->_status = init;
+		}
+		return t;
+	}
+
+	void purge_os_task(os_task** t) {
+		free(*t);
+		*t = 0;
+	}
 }
 
 namespace test {
@@ -36,14 +52,46 @@ namespace test {
 	TEST(os_task) {
 
 		using namespace ostask;
-		os_task task_0 = { 0, fn0, init };
-		os_task* t = &task_0;
 
-		assert(t->_status == ostask::init);
-		assert(complete(t) == ostask::completed);
-		assert(pending(t) == FALSE);
-		reset(t);
-		assert(pending(t) == TRUE);
+		SUBTEST(INIT)
+		{
+			os_task t0 = { 0, fn0, init };
+			os_task* t = &t0;
+
+			assert(t->_status == ostask::init);
+			assert(complete(t) == ostask::completed);
+			assert(pending(t) == FALSE);
+			reset(t);
+			assert(pending(t) == TRUE);
+		}
+
+		SUBTEST(CREATE_TASK)
+		{
+			os_task* t1 = create_os_task(1, fn1);
+
+			assert(t1 != 0);
+			assert(t1->_id == 1);
+			assert(t1->_status == init);
+			assert(t1->_task == fn1);
+
+			purge_os_task(&t1);
+			assert(t1 == 0);
+
+			os_task* t2 = create_os_task(2, fn0);
+
+			assert(t2 != 0);
+			assert(t2->_id == 2);
+			assert(t2->_status == init);
+			assert(t2->_task == fn0);
+
+			assert(pending(t2) == TRUE);
+			assert(complete(t2) == completed);
+			assert(pending(t2) == FALSE);
+			reset(t2);
+			assert(pending(t2) == TRUE);
+			assert(complete(t2) == completed);
+			assert(pending(t2) == FALSE);
+		}
 
 		return;
 	}
